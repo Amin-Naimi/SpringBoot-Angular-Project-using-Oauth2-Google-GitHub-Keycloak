830e1e12441e5d4fc13e66ae636a1058d68c3894
Mohamed <ahnbi32@gmail.com>
2023-12-05 22:48:50 +0100

    adding Security Config class

--- src/main/java/com/Mohamed/customerapp/security/SecurityConfig.java
+++ src/main/java/com/Mohamed/customerapp/security/SecurityConfig.java
@@ -1,4 +1,22 @@
 package com.Mohamed.customerapp.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)throws Exception{
+        return httpSecurity
+                .csrf(Customizer.withDefaults())// utliser la configuration par defaut
+                .authorizeHttpRequests(ar->ar.requestMatchers("/","/webjars/**","/h2-console/**").permitAll())
+                .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .build();
+    }
 }
